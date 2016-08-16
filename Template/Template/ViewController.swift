03436ccaabb4b4ad0524557a9efb6d883c7cf100
Krishnendra Prakash <krishnep@in.ibm.com>
2016-08-17 01:05:09 +0530

    Firt commit

---
+++ Template/Template/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Template
+//
+//  Created by krishnendra on 8/17/16.
+//  Copyright © 2016 krishnendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
